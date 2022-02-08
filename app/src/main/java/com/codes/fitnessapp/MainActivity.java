30f76bbd88b4cc1cfbfd51d2897dfed880c351c5
shivamchaudhary-code <shivamchaudhary177@gmail.com>
2022-02-09 00:35:32 +0530

    this is fitness app project

---
+++ app/src/main/java/com/codes/fitnessapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.codes.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       //Toolbar toolbar = findViewById(R.id.toolBar1);
+      //setSupportActionBar(toolbar);
+
+
+        button1 = findViewById(R.id.startyoga1);
+        button2 = findViewById(R.id.startyoga2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+    public void beforage18(View view) {
+        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void afterage18(View view) {
+        Intent intent = new Intent(MainActivity.this,SecondActivity2.class);
+        startActivity(intent);
+    }
+
+    public void food(View view) {
+        Intent intent = new Intent(MainActivity.this,FoodActivity.class);
+        startActivity(intent);
+    }
+}
